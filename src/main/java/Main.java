10eb373c25e9bc1b55f9c26cfedb08197b789e49
niushiyu <niushy@dazhuanjia.com>
2018-05-28 17:40:49 +0800

    添加时间记录
    Signed-off-by: niushiyu <niushy@dazhuanjia.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,8 @@ public class Main {
 
     public static void main(String[] args) {
         WorkerChain workerChain = new WorkerChain();
-        workerChain.register(new LoginWorker(httpClient, LoginUser.ACCOUNT_FOR_ORIGIN)).register(new OriginWorker(httpClient)).doChain();
-        workerChain.register(new LoginWorker(httpClient, LoginUser.ACCOUNT_FOR_DATA)).register(new SmartWorker(httpClient)).doChain();
+        workerChain.register(new LoginWorker(httpClient, LoginUser.ACCOUNT_FOR_ORIGIN)).register(new LoginWorker(httpClient, LoginUser.ACCOUNT_FOR_DATA)).doChain();
+//        workerChain.register(new LoginWorker(httpClient, LoginUser.ACCOUNT_FOR_ORIGIN)).register(new OriginWorker(httpClient)).doChain();
+//        workerChain.register(new LoginWorker(httpClient, LoginUser.ACCOUNT_FOR_DATA)).register(new SmartWorker(httpClient)).doChain();
     }
 }
--- src/main/java/com/kiki/worker/WorkerChain.java
+++ src/main/java/com/kiki/worker/WorkerChain.java
@@ -1,9 +1,14 @@
 package com.kiki.worker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class WorkerChain {
+    private static Logger logger = LoggerFactory.getLogger(WorkerChain.class);
+
     private List<AbstractWorker> workerList = new ArrayList<>();
 
     public WorkerChain register(AbstractWorker worker) {
@@ -12,9 +17,13 @@ public class WorkerChain {
     }
 
     public void doChain() {
+        long start = System.currentTimeMillis();
         for (AbstractWorker worker : workerList) {
             worker.work();
         }
+        long end = System.currentTimeMillis();
+        double cost = (end - start) / (1000.00);
+        logger.info("完成任务链共耗费:" + cost + "s.");
     }
 
 }
